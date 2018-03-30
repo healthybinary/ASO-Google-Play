e716e4a1c90f191445f027ffa3b8fb352326d7db
Ayoub Fletcher <ayoubwellsten@gmail.com>
2018-03-30 10:23:42 +0100

    Create red-border.user.js

---
+++ red-border.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Red Border
+// @description A super simple user script with an unobtrusive way of being clear that it's running.
+// @namespace   test
+// @include     http*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+document.body.style.border = '3px dashed red';
